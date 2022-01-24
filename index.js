c6e4a6f802174613cba8ce205eec2c57c3ba173a
sonyideaology <Sony@ideaology.io>
2022-01-24 13:16:34 +0400

    api

--- index.js
+++ index.js
@@ -474,4 +474,4 @@ app.get('/api/symbol' ,async function (req , res){
 //  const express = require('express')
 // const app = express()
 //  app.get('/', function (req, res) {  res.send('Hello World')})
- app.listen(3000)
+ app.listen(3000);
